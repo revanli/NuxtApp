2d712739f86482315c5460230390f4c914bb66af
liyufan1 <liyufan1@huya.com>
2022-12-29 22:54:28 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  head() {
+    return {
+      title:  '单独页面 title',
+      meta: [{
+        hid: 'description',
+        name: 'description',
+        content: '单独页面Meta description'
+      },
+      {
+          hid: 'keywords',
+          name: 'keywords',
+          content: '单独页面Meta keywords'
+      }]
+    }
+  }
+})
+</script>
